c8272e4b6d86e8913e2038a0bb7a0ecbfbd7329e
SolomonNnamani <solomonnnamani01@gmail.com>
2025-05-08 14:36:16 +0100

    Updated API URLs to point to live backend

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://tasksflow01.netlify.app/", // allow your frontend domain
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
